fbfe282c60c64ba7e1eaa90588171d760079273f
Dan <dan@example.org>
2016-02-20 23:06:31 -0500

    Added streaminfo

--- streaminfo.py
+++ streaminfo.py
@@ -1,3 +1,3 @@
-STREAMKEY = 'live_116183327_n6ZdxJUprzlHJJvmRuh3WczfHx27yt'
-OAUTH = 'oauth:sldi6ezn53mrewhnn3u5mvhslc3b8p'
-USERNAME = 'twitchdrawsrichards'
+STREAMKEY = 'your twitch streamkey'
+OAUTH = 'your oauth'
+USERNAME = 'your username'
